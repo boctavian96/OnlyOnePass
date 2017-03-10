ed39a3426dcffeb1bd4c2b739e8a546c8ae15c77
Adelina <Adelina@DESKTOP-ES50VTO>
2017-03-10 22:06:42 +0200

    DaatWriter without append when writing 2 files

--- src/database/DataWritter.java
+++ src/database/DataWritter.java
@@ -5,10 +5,39 @@
  */
 package database;
 
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 /**
  *
- * @author Octavian
+ * @author Adeleena
  */
 public class DataWritter {
+  	private String pass;
+	private String site;
+	/**
+	 * @param pass
+	 * @param site
+	 */
+	public DataWritter(String pass, String site) {
+		this.pass = pass;
+		this.site = site;
+	}
+	void WriteMyData() throws FileNotFoundException
+	{
+            PrintWriter siteOut;
+              try (PrintWriter passOut = new PrintWriter("passes.txt")) {
+                  passOut.println((String)this.pass);
+                  siteOut = new PrintWriter("sites.txt");
+                  siteOut.println((String)this.site);
+              }
+		 siteOut.close();
+	}
+
+    
+    
+    
+    
+    
     
 }
