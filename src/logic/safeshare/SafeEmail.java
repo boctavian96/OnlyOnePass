f4686f88a7a83bc8e874628433d88f8abfc52d96
boctavian96 <boctavian96@yahoo.com>
2017-04-10 12:06:56 +0300

    v0.3.125

--- src/logic/safeshare/SafeEmail.java
+++ src/logic/safeshare/SafeEmail.java
@@ -5,10 +5,10 @@
  */
 package logic.safeshare;
 
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
+//import java.util.*;
+//import javax.mail.*;
+//import javax.mail.internet.*;
+//import javax.activation.*;
 
 /**
  *
